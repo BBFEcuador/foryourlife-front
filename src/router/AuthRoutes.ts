1d64d880212c0e0be68416d4c780efff6419cb40
PROPHET2350 <jorgeluis33ariosa@gmail.com>
2025-02-13 16:43:59 -0500

    create route form

--- src/router/AuthRoutes.ts
+++ src/router/AuthRoutes.ts
@@ -11,29 +11,9 @@ const PublicRoutes = {
       component: () => import('@/views/authentication/LoginPage.vue')
     },
     {
-      name: 'Login',
-      path: '/login1',
-      component: () => import('@/views/authentication/auth1/LoginPage1.vue')
-    },
-    {
-      name: 'Register',
-      path: '/register1',
-      component: () => import('@/views/authentication/auth1/RegisterPage1.vue')
-    },
-    {
-      name: 'Forgot Password',
-      path: '/forgot-pwd1',
-      component: () => import('@/views/authentication/auth1/ForgotPwd1.vue')
-    },
-    {
-      name: 'Reset Password',
-      path: '/reset-pwd1',
-      component: () => import('@/views/authentication/auth1/ResetPwd1.vue')
-    },
-    {
-      name: 'Error 404',
-      path: '/error',
-      component: () => import('@/views/pages/maintenance/error/Error404Page.vue')
+      name: 'Authentication',
+      path: '/register/:token',
+      component: () => import('@/views/authentication/ParticipantsRegister.vue')
     }
   ]
 };
