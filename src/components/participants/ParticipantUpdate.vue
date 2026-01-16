8ac290bd6350a8005dece0fa62ac0d6ae93865d1
PROPHET2350 <jorgeluis33ariosa@gmail.com>
2026-01-16 15:25:27 -0500

    fix update participant

--- src/components/participants/ParticipantUpdate.vue
+++ src/components/participants/ParticipantUpdate.vue
@@ -56,8 +56,8 @@ const formattedBirthday = computed({
         <v-text-field v-model="props.participant.user.lastname1" label="Apellido 1" variant="outlined" />
         <v-text-field v-model="props.participant.user.lastname2" label="Apellido 2" variant="outlined" />
       </div>
-      <v-text-field v-model="props.participant.email" label="Correo Electrónico" variant="outlined" />
-      <v-text-field v-model="props.participant.phone" label="Teléfono" variant="outlined" />
+      <v-text-field v-model="props.participant.user.email" label="Correo Electrónico" variant="outlined" />
+      <v-text-field v-model="props.participant.user.phone" label="Teléfono" variant="outlined" />
       <div class="d-flex tw:gap-x-2">
         <v-text-field v-model="props.participant.profile.dni" label="Cédula" variant="outlined" />
         <v-text-field v-model="props.participant.profile.civilStatus" label="Estado Civil" variant="outlined" />
@@ -67,17 +67,11 @@ const formattedBirthday = computed({
           <VDateInput v-model="formattedBirthday" label="Fecha de nacimiento" variant="outlined"></VDateInput>
         </v-col>
         <v-col cols="12" md="6">
-          <v-select
-            :items="[
-              { label: 'Femenino', value: 'M' },
-              { label: 'Masculino', value: 'H' }
-            ]"
-            item-title="label"
-            item-value="value"
-            label="Género"
-            variant="outlined"
-            v-model="props.participant.profile.gender"
-          />
+          <v-select :items="[
+            { label: 'Femenino', value: 'M' },
+            { label: 'Masculino', value: 'H' }
+          ]" item-title="label" item-value="value" label="Género" variant="outlined"
+            v-model="props.participant.profile.gender" />
         </v-col>
       </v-row>
       <v-text-field v-model="props.participant.profile.occupation" label="Ocupación" variant="outlined" />
@@ -87,7 +81,8 @@ const formattedBirthday = computed({
       <v-text-field v-model="props.participant.profile.address" label="Dirección" variant="outlined" />
 
       <v-spacer />
-      <v-btn color="primary" @click="onSubmitParticipant" :loading="updateParticipantMutation.isPending.value">Actualizar</v-btn>
+      <v-btn color="primary" @click="onSubmitParticipant"
+        :loading="updateParticipantMutation.isPending.value">Actualizar</v-btn>
     </v-card-item>
   </v-card>
 </template>
